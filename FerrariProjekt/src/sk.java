b74bee933a4c8c028fb9bdacecb0ac548b5869eb
Søren Kaalund <klund89@gmail.com>
2020-04-24 13:18:13 +0200

    fejl

--- FerrariProjekt/src/sk.java
+++ FerrariProjekt/src/sk.java
@@ -1,12 +1,8 @@
 
 public class sk {
-<<<<<<< HEAD
-//Skulle gerne kun være på min branch
-//eller hvad sker der lige
-=======
-//Skulle gerne kun være på in branch
+
 	public static void Main(String[] args) {
 		
 	}
->>>>>>> master
+
 }
