69a8e7f7d8ced8a16523de98a39f51cf3a5c186a
Daniel Bjerre <danielbmadsen93@gmail.com>
2020-05-11 13:13:56 +0200

    GUI Opret
    oprettet presentation package
    oprettet tomme presentation klasser
    oprettet main.java

--- FerrariProjekt/src/presentation/Main.java
+++ FerrariProjekt/src/presentation/Main.java
@@ -1,9 +1,15 @@
+package presentation;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
 }
