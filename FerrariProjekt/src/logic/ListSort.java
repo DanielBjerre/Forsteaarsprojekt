ef45b23a469ee2d698f8cccc242b33bc7c7d0f09
Kaalund <klund89@gmail.com>
2020-05-24 19:23:23 +0200

    startet on CarSort

--- FerrariProjekt/src/logic/ListSort.java
+++ FerrariProjekt/src/logic/ListSort.java
@@ -2,13 +2,14 @@ package logic;
 
 import java.util.ArrayList;
 
+import entities.Car;
 import entities.Offer;
 
 public class ListSort {
 
 	public ArrayList<Offer> sortOffer(ArrayList<Offer> arrayList, String criteria, String value) {
 		ArrayList<Offer> tempList = new ArrayList<Offer>();
-		
+
 		for (Offer offer : arrayList) {
 			switch (criteria) {
 				case "S�lger":
@@ -46,6 +47,25 @@ public class ListSort {
 					tempList.add(offer);
 
 					break;
+			}
+		}
+		return tempList;
+	}
+
+	public ArrayList<Car> sortCar(ArrayList<Car> arrayList, String used, String model) {
+		boolean bUsed = false;
+		switch (used) {
+			case "Ny":
+				bUsed = false;
+				break;
+			case "Brugt":
+				bUsed = true;
+				break;
+		}
+		ArrayList<Car> tempList = new ArrayList<>();
+		for (Car car : arrayList) {
+			if (car.isUsed() == bUsed && car.getModel().equals(model)) {
+				tempList.add(car);
 			}
 		}
 		return tempList;
--- FerrariProjekt/src/logic/prototypeListFillWhitoutDupllicates.java
+++ FerrariProjekt/src/logic/prototypeListFillWhitoutDupllicates.java
@@ -10,12 +10,8 @@ public class prototypeListFillWhitoutDupllicates {
     ArrayList<String> model = new ArrayList<>();
     ArrayList<String> modelYear = new ArrayList<>();
     ArrayList<String> color = new ArrayList<>();
-    public static void main(String[] args) {
-        prototypeListFillWhitoutDupllicates pfwd = new prototypeListFillWhitoutDupllicates();
-        pfwd.run();    
-    }
 
-    public void run()
+    public void run(ArrayList<Car> arrayList)
     {
         ArrayList<Car> car = new ReadCar().findCars();
         for (Car item : car) {
@@ -28,6 +24,18 @@ public class prototypeListFillWhitoutDupllicates {
     private void fillList(ArrayList<String> taget, String value)
     {
         if (!taget.contains(value)) { taget.add(value); }
+    }
+
+    public ArrayList<String> getModel() {
+        return model;
+    }
+
+    public ArrayList<String> getModelYear() {
+        return modelYear;
+    }
+
+    public ArrayList<String> getColor() {
+        return color;
     }
 
 }
